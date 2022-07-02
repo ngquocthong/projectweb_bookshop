8bd2f83553035dca9e1412c8228cecef50be95a1
Tan Huynh <104268347+tan00444@users.noreply.github.com>
2022-07-02 13:30:47 +0700

    create CRUD Author and Cart

---
+++ src/Form/AuthorType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Author;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AuthorType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username')
+            ->add('password')
+            ->add('email')
+            ->add('phone')
+            ->add('fullname')
+            ->add('gender')
+            ->add('dateofbirth')
+            ->add('wallet')
+            ->add('rate')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Author::class,
+        ]);
+    }
+}
